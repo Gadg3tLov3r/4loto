0fe6cb5bbf4624b8d6f9e8e2735a25b789183cac
Gadg3tLov3r <dev2@cmiyc.pro>
2025-09-24 03:16:31 +0600

    Update SpecialLotteryCard component to include ticketsSold and ended properties, enhancing display logic for ticket status and draw timing. Add new image assets for special lotteries and update lottery data with new titles, descriptions, and draw dates.

--- components/special-lottery-card.tsx
+++ components/special-lottery-card.tsx
@@ -1,66 +1,98 @@
+import Image from "next/image";
+
 interface SpecialLotteryCardProps {
   title: string;
   description: string;
   ticketsLeft: number;
+  ticketsSold: number;
   prizeAmount: string;
   drawDate: string;
   backgroundImage?: string;
   buttonGradient?: string;
+  ended: boolean;
 }
 
 export function SpecialLotteryCard({
   title,
   description,
   ticketsLeft,
+  ticketsSold,
   prizeAmount,
   drawDate,
   backgroundImage,
   buttonGradient,
+  ended,
 }: SpecialLotteryCardProps) {
   return (
-    <div className="p-4 pt-0 border border-[#231968] rounded-[40px] h-[464px] flex flex-col">
-      <div className="px-15">
-        <div className="bg-white/8 backdrop-blur-3xl py-[9px] px-[24px] rounded-b-[30px] flex justify-center items-center">
-          <button className="border border-[#44398D] py-[5px] px-[11.17px] rounded-[14.89px] text-[14.89px] hover:bg-white/10 transition-colors">
-            See Results
-          </button>
+    <div className="p-4 pt-0 border border-[#231968] rounded-[40px]  h-[464px] flex flex-col">
+      <div className="flex justify-center">
+        <div className="bg-white/8 backdrop-blur-3xl py-[9px] px-[24px] h-[52px] min-w-[200px] rounded-b-[30px] flex justify-center items-center">
+          {ended ? (
+            <button className="border border-[#44398D] py-[5px] px-[11.17px] rounded-[14.89px] text-[14.89px] hover:bg-white/10 transition-colors">
+              See Results
+            </button>
+          ) : (
+            <div className="flex">
+              <div className="border border-[#44398D] py-[5px] px-[11.17px] rounded-[14.89px] text-[14.89px]">
+                5d
+              </div>
+              <div className="border border-[#44398D] py-[5px] px-[11.17px] rounded-[14.89px] text-[14.89px]">
+                12h
+              </div>
+              <div className="border border-[#44398D] py-[5px] px-[11.17px] rounded-[14.89px] text-[14.89px]">
+                55m
+              </div>
+              <div className="border border-[#44398D] py-[5px] px-[11.17px] rounded-[14.89px] text-[14.89px]">
+                34s
+              </div>
+            </div>
+          )}
         </div>
       </div>
-      <div
-        className="flex flex-col flex-1 rounded-3xl bg-center bg-cover"
-        style={{
-          backgroundImage: backgroundImage
-            ? `url('${backgroundImage}')`
-            : undefined,
-        }}
-      >
-        <div className="flex-1"></div>
-
-        <div className="grid grid-cols-3">
-          <div className="text-center bg-[#130E38] p-2 rounded-t-3xl">
-            <div className="text-xs text-white/40">Tickets Left</div>
-            <div className="text-sm text-white/80">
-              {ticketsLeft.toLocaleString()}
+      <div className="flex-1 rounded-b-[30px] relative flex">
+        <div
+          className="bg-cover bg-top mx-6 flex-1"
+          style={{
+            backgroundImage: `url('${backgroundImage}')`,
+          }}
+        ></div>
+        <div className="absolute bottom-0 left-0 right-0">
+          <div className="grid grid-cols-3">
+            <div className="text-center bg-[#130E38] p-2 rounded-t-3xl">
+              <div className="text-[8px] text-white/40">
+                Tickets {ended ? "Sold" : "Left"}
+              </div>
+              <div className="text-xs text-white/80">
+                {ended
+                  ? ticketsSold
+                  : ticketsLeft === 0
+                  ? "Sold out"
+                  : ticketsLeft}
+              </div>
+            </div>
+            <div className="bg-white/8 backdrop-blur-xl rounded-3xl flex justify-center items-center font-bold h-[54px] min-w-[118px]">
+              ${prizeAmount}
+            </div>
+            <div className="text-center bg-[#130E38] p-2 rounded-t-3xl">
+              <div className="text-[8px] text-white/40">
+                Draw {ended ? "Ended" : "Starts"}
+              </div>
+              <div className="text-white/80 text-xs">
+                {new Date(drawDate).toLocaleDateString()}
+              </div>
             </div>
           </div>
-          <div className="bg-white/1 backdrop-blur-lg rounded-3xl flex justify-center items-center font-bold">
-            ${prizeAmount}
+          <div className="bg-[#130E38] flex flex-col gap-2 p-4 rounded-b-3xl">
+            <div className="text-center">
+              <h2 className="text-white/80">{title}</h2>
+              <p className="text-xs text-white/40">{description}</p>
+            </div>
+            <button
+              className={`h-[50px] ${buttonGradient} px-8 py-2 font-bold rounded-3xl self-center`}
+            >
+              Buy Tickets
+            </button>
           </div>
-          <div className="text-center bg-[#130E38] p-2 rounded-t-3xl">
-            <div className="text-xs text-white/40">Draw Starts</div>
-            <div className="text-white/80 text-sm">{drawDate}</div>
-          </div>
-        </div>
-        <div className="bg-[#130E38] flex flex-col gap-2 p-4 rounded-b-3xl">
-          <div className="text-center">
-            <h2 className="text-lg text-white/80">{title}</h2>
-            <p className="text-sm text-white/40">{description}</p>
-          </div>
-          <button
-            className={`h-[50px] ${buttonGradient} px-8 py-2 font-bold rounded-3xl self-center`}
-          >
-            Buy Tickets
-          </button>
         </div>
       </div>
     </div>
--- sections/special-lotteries.tsx
+++ sections/special-lotteries.tsx
@@ -4,38 +4,46 @@ const data = [
   {
     title: "Happy Easter Draw",
     description: "Hop into Easter magic — win big!",
-    ticketsLeft: 10000,
+    ticketsLeft: 0,
+    ticketsSold: 9473,
     prizeAmount: "55000",
-    drawDate: "2025-03-08",
-    backgroundImage: "/rabbit.png",
+    drawDate: "2025-04-12",
+    backgroundImage: "/rabbit2.png",
     buttonGradient: "bg-gradient-to-b from-[#DFCDD9] to-[#130E38]",
+    ended: true,
   },
   {
-    title: "Happy Easter Draw",
-    description: "Hop into Easter magic — win big!",
-    ticketsLeft: 10000,
+    title: "Buddha's Birthday",
+    description: "Bloom with serenity — special prize awaits!",
+    ticketsLeft: 734562,
+    ticketsSold: 10000,
     prizeAmount: "55000",
-    drawDate: "2025-03-08",
-    backgroundImage: "/buddha.png",
+    drawDate: "2025-05-05",
+    backgroundImage: "/buddha2.png",
     buttonGradient: "bg-gradient-to-b from-[#F1991E] to-[#130E38]",
+    ended: false,
   },
   {
-    title: "Happy Easter Draw",
-    description: "Hop into Easter magic — win big!",
+    title: "International Women's Day",
+    description: "Empower your luck — win on Women's Day!",
     ticketsLeft: 10000,
+    ticketsSold: 10000,
     prizeAmount: "55000",
     drawDate: "2025-03-08",
-    backgroundImage: "/flower.png",
+    backgroundImage: "/flower2.png",
     buttonGradient: "bg-gradient-to-b from-[#F800B7] to-[#130E38]",
+    ended: false,
   },
   {
-    title: "Happy Easter Draw",
-    description: "Hop into Easter magic — win big!",
-    ticketsLeft: 10000,
-    prizeAmount: "55000",
-    drawDate: "2025-03-08",
-    backgroundImage: "witch.png",
+    title: "International Fool’s Day",
+    description: "You can win... Or not",
+    ticketsLeft: 0,
+    ticketsSold: 10000,
+    prizeAmount: "3000000",
+    drawDate: "2025-04-01",
+    backgroundImage: "/witch2.png",
     buttonGradient: "bg-gradient-to-b from-[#2FFF6C] to-[#130E38]",
+    ended: false,
   },
 ];
 
@@ -50,10 +58,12 @@ export const SpecialLotteriesSection = () => {
             title={item.title}
             description={item.description}
             ticketsLeft={item.ticketsLeft}
+            ticketsSold={item.ticketsSold}
             prizeAmount={item.prizeAmount}
             drawDate={item.drawDate}
             backgroundImage={item.backgroundImage}
             buttonGradient={item.buttonGradient}
+            ended={item.ended}
           />
         ))}
       </div>
